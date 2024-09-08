162f352f65affab25f1369387876e9c75ef0b053
Anatolii Petrov <hello@aswitch.me>
2024-09-08 12:07:02 +0200

    Initial Commit

---
+++ AvitoUnsplashTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoUnsplashTest
+//
+//  Created by Anatoliy Petrov on 8.9.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
