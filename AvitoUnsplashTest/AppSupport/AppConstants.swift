cc3faabc8f0342cf2330fdff20384d33a3a5fe7c
Anatolii Petrov <hello@aswitch.me>
2024-09-12 21:53:03 +0200

    Added Constants

---
+++ AvitoUnsplashTest/AppSupport/AppConstants.swift
@@ -0,0 +1,15 @@
+//
+//  AppConstants.swift
+//  AvitoUnsplashTest
+//
+//  Created by Anatoliy Petrov on 8.9.24..
+//
+
+import Foundation
+
+struct Constants {
+    struct API {
+        static let baseURL = "https://api.unsplash.com/search/photos?"
+        static let clientID = "API_KEY"
+    }
+}
